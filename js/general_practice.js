e9321ed622ec53cac6bcf582ea152802e226e866
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-08-26 07:42:57 -0500

    Stand in Line

--- js/general_practice.js
+++ js/general_practice.js
@@ -89,3 +89,58 @@ function isArray(array) {
 }
 console.log(isArray(myArray))
 console.log(isArray(28));
+
+
+//Practice With functions
+//Basic Functions and variable declarations
+//Can we put a list of radius in an array, and have this function print out all the circumference?
+
+let arrayOfRadii = [2, 4, 6, 8, 10, 12]
+
+//     function processRadii(array) {
+//         function calculateCircumference(radius) {
+//             for(let i = 0; i < array.length; i++){
+//
+//                 let radius = array[i];
+//
+//         }
+//         const pi = 3.14149;
+//         return 2 * pi * radius;
+//     }
+//
+// }
+// console.log(calculateCircumference(arrayOfRadii));
+
+function calculateCircumference(radius) {
+    const pi = 3.14159;
+    let circumference = 2 * pi * radius;
+    return circumference.toFixed(2);
+}
+
+console.log(calculateCircumference(13));
+
+function milesToKilometers(miles) {
+    return miles * 1.609;
+}
+
+
+
+let changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+console.log(changed);
+
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift();
+}
+
+let testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
